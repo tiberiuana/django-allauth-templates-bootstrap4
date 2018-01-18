9e01ab5ff6c7462bb3e496788ccabfdfb61f3e82
Tiberiu Ana <tiberiu@tiberiuana.com>
2018-01-18 07:06:57 +0100

    Track existing files

---
+++ README.rst
@@ -0,0 +1,13 @@
+==========================
+django-allauth-helper
+==========================
+
+Template pack for django-allauth using Bootstrap 4.
+
+Installation
+==============
+
+After successfully installing and configuring django-allauth, add `allauth_bootstrap4` to your INSTALLED_APPS *above* `allauth`.
+
+The template pack does *not* distribute Bootstrap, instead it expects a Bootstrap 4 build in `STATIC_DIR/bootstrap/`. 
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+METADATA = {
+  "name": 'django-allauth-templates-bootstrap4',
+  "version": "0.34.1",
+  "author": "Tiberiu Ana",
+  "author_email": "tiberiu@tiberiuana.com",
+  "description": "Bootstrap4 template pack for django-allauth",
+  "url": "http://github.com/tiberiuana/django-allauth-templates-bootstrap4",
+  "keywords": "django auth account social openid twitter facebook oauth registration",
+  "install_requires": [
+    "django-allauth >= 0.34.0"
+  ],
+  "license": "MIT",
+  "classifiers": [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Environment :: Web Environment',
+    'Topic :: Internet',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Framework :: Django',
+    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.0',
+  ],
+}
+
+setup(**METADATA)
